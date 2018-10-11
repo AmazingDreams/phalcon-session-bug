0698f239bb8054f8e3a67dee941fc66307325425
Dennis Ruhe <dennis.ruhe@troistudio.com>
2018-10-11 12:03:38 +0200

    More version

--- bug.php
+++ bug.php
@@ -1,6 +1,8 @@
 <?php
 
-echo "Running test for ".phpversion().PHP_EOL;
+$v = phpversion()." - ".\Phalcon\Version::get();
+
+echo "Running test for ".$v.PHP_EOL;
 
 $session = new \Phalcon\Session\Adapter\Files();
 echo "Session instantiated".PHP_EOL;
@@ -11,4 +13,4 @@ echo "Session started".PHP_EOL;
 $session->set('test', 'test');
 echo "Session key set".PHP_EOL;
 
-echo "\033[32m ".phpversion()." EXITED SUCCESSFULLY\033[0m".PHP_EOL;
+echo "\033[32m ".$v." EXITED SUCCESSFULLY\033[0m".PHP_EOL;
